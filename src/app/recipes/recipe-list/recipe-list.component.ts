13f465ee2a40349df3461e247eac4cb528c74790
Mario Morales <mario.morales@devsu.com>
2021-09-29 12:31:17 -0500

    added ingredient model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+   selector: 'app-recipe-list',
+   templateUrl: './recipe-list.component.html',
+   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+   recipes: Recipe[] = [
+      new Recipe(
+         'Arroz con pollo',
+         'A test recipe description',
+         'https://t2.rg.ltmcdn.com/es/images/4/5/7/arroz_con_pollo_panameno_8754_600.jpg'
+      ),
+      new Recipe(
+         'Arroz con pollo',
+         'A test recipe description',
+         'https://t2.rg.ltmcdn.com/es/images/4/5/7/arroz_con_pollo_panameno_8754_600.jpg'
+      )
+   ];
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+   }
 
 }
