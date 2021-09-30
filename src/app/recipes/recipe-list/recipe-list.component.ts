442ef8c440d6b840fa0f3729037f229b41a09f88
Mario Morales <mario.morales@devsu.com>
2021-09-30 12:34:16 -0500

    RecipeService added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
    selector: 'app-recipe-list',
@@ -7,25 +8,10 @@ import { Recipe } from '../recipe.model';
    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-   @Output() recipeSelected = new EventEmitter<Recipe>();
-   recipes: Recipe[] = [
-      new Recipe(
-         'Arroz con pollo',
-         'A test recipe description',
-         'https://t2.rg.ltmcdn.com/es/images/4/5/7/arroz_con_pollo_panameno_8754_600.jpg'
-      ),
-      new Recipe(
-         'Sancocho',
-         'A sancocho recipe description',
-         'https://www.196flavors.com/wp-content/uploads/2018/10/sancocho-3.jpg'
-      )
-   ];
-   constructor() { }
+   recipes: Recipe[];
+   constructor(private recipeService: RecipeService) { }
 
    ngOnInit(): void {
-   }
-
-   onRecipeSelected(selectedRecipe: Recipe) {
-      this.recipeSelected.emit(selectedRecipe);
+      this.recipes = this.recipeService.getRecipes();
    }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,23 @@
-export class RecipeService {
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+   recipeSelected = new EventEmitter<Recipe>();
+
+   private recipes: Recipe[] = [
+      new Recipe(
+         'Arroz con pollo',
+         'A test recipe description',
+         'https://t2.rg.ltmcdn.com/es/images/4/5/7/arroz_con_pollo_panameno_8754_600.jpg'
+      ),
+      new Recipe(
+         'Sancocho',
+         'A sancocho recipe description',
+         'https://www.196flavors.com/wp-content/uploads/2018/10/sancocho-3.jpg'
+      )
+   ];
+
+   getRecipes() {
+      return [...this.recipes];
+   }
 }
