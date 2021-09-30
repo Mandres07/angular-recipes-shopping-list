ac11c6bd7a65d1909beac2e6a2dea735f41ee13f
Mario Morales <mario.morales@devsu.com>
2021-09-30 12:50:00 -0500

    added ShoppingListService

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
-export class ShoppingListService {
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
+export class ShoppingListService {
+   ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+   private ingredients: Ingredient[] = [
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomatoes', 10),
+      new Ingredient('Rice', 5),
+   ];
+
+   getIngredients() {
+      return [...this.ingredients];
+   }
+
+   addIngredient(name: string, amount: number) {
+      this.ingredients.push(new Ingredient(name, amount));
+      this.ingredientsChanged.emit([...this.ingredients]);
+   }
 }
